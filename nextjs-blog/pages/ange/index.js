9a5b3374a753065481200a61ad18f8519307f79c
angeuwase <angeuwasedev@gmail.com>
2021-08-16 10:20:59 +0100

    crash course completed

---
+++ nextjs-blog/pages/ange/index.js
@@ -0,0 +1,22 @@
+
+import Link from 'next/link'
+
+
+export default function FirstPost() {
+    
+    return (
+        <div>
+                <h1>Hi Ange!</h1>
+
+<h1 className="title">
+Return{' '}
+<Link href="/">
+  <a>Home!</a>
+</Link>
+</h1>
+            
+        </div>
+    
+
+        )
+  }
